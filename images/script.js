6a5a08c9d319b3d54e697eea56dc7f36b1d5d2e5
Joo <joo.yoon@daumkakao.com>
2015-09-24 18:15:05 +0900

    social share

--- images/script.js
+++ images/script.js
@@ -104,10 +104,10 @@
 
 	Area.Comment = (function() {
 		var $btnOpen = $(".btn_reply"),
-			$listReply = $(".list_reply");
+			$fieldReply = $(".fld_reply");
 
 		var changeStatus = function() {
-			if ($listReply.is(":show")) {
+			if ($fieldReply.is(":visible")) {
 				$btnOpen.removeClass("on");
 			} else {
 				$btnOpen.addClass("on");
@@ -115,7 +115,7 @@
 		};
 
 		var init = function() {
-			if ($listReply.is(":show")) {
+			if ($fieldReply.is(":visible")) {
 				$btnOpen.addClass("on");
 			}
 
@@ -127,6 +127,57 @@
 		}
 	})();
 
+	Area.SocialShare = (function() {
+
+		var SOCIAL_SHARE = {
+			kakaostory: {
+				url: "https://story.kakao.com/share",
+				width: 640,
+				height: 400,
+				makeShareUrl: function(url) {
+					return this.url + "?url=" + encodeURIComponent(url);
+				}
+			},
+			facebook: {
+				url: "https://www.facebook.com/sharer/sharer.php",
+				width: 640,
+				height: 400,
+				makeShareUrl: function(url) {
+					return this.url + "?display=popup&u=" + encodeURIComponent(url);
+				}
+			},
+			twitter: {
+				url: "https://twitter.com/intent/tweet",
+				width: 640,
+				height: 400,
+				makeShareUrl: function(url) {
+					return this.url + "?url=" + encodeURIComponent(url);
+				}
+			}
+		};
+
+
+		var onClick = function(e) {
+			var $this = $(this),
+				service = SOCIAL_SHARE[$this.data("service")],
+				url = location.href;
+
+			if (service) {
+				e.preventDefault();
+				window.open(service.makeShareUrl(url), "", "width=" + service.width + ", height=" + service.height);
+			}
+		};
+
+
+		var init = function() {
+			$(".list_share").on("click", "a", onClick);
+		};
+
+		return {
+			init: init
+		}
+	})();
+
 	Area.init = function() {
 		Area.Window.init();
 		Area.Skin.init();
@@ -134,6 +185,7 @@
 		Area.Category.init();
 		Area.Search.init();
 		Area.Comment.init();
+		Area.SocialShare.init();
 	};
 
 	$.Area = {
